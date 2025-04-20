79f1278266cfc94f32f2c7342488d65145d59884
taetae0707 <taetaeky07@gmail.com>
2025-04-20 16:19:51 +0900

    layout에  footer컴포넌트 추가

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,4 +1,5 @@
 import Header from '@components/Layout/Header/Header'
+import { Footer } from '@components/Layout/Footer/Footer'
 import { Outlet } from 'react-router-dom'
 
 interface LayoutProps {
@@ -10,6 +11,7 @@ const Layout: React.FC<LayoutProps> = () => {
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   )
 }
