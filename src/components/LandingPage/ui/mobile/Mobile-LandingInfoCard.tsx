fff3578e505e282199209281bc2d11317eac0ffb
taetae0707 <taetaeky07@gmail.com>
2025-05-01 20:28:51 +0900

    refactor: landing page 인포카드 ui변경

--- src/components/LandingPage/ui/mobile/Mobile-LandingInfoCard.tsx
+++ src/components/LandingPage/ui/mobile/Mobile-LandingInfoCard.tsx
@@ -16,15 +16,17 @@ export function MobileLandingInfoCard({ infoText, imgSrc, onClick }: LandingInfo
 }
 
 const LandingInfoCardContainer = styled.div`
-  width: 30%;
-  height: 28.125rem;
+  gap: 1.625rem;
+  width: 100%;
+  height: 30rem;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  padding: 20px;
-  border-radius: 22px;
+  padding: 1.25rem 20px;
+  border-radius: 1.375rem;
   background: #fff;
+
   @media (max-width: 1320px) {
     height: 17rem;
     gap: 1rem;
@@ -37,8 +39,9 @@ const LandingInfoCardContainer = styled.div`
 `
 
 const InfoCardImage = styled.img`
-  width: 270px;
-  height: 210px;
+  width: 65%;
+  height: 50%;
+
   @media (max-width: 1024px) {
     width: 30%;
     height: 45%;
@@ -50,12 +53,11 @@ const InfoCardImage = styled.img`
 `
 
 const InfoCardText = styled.div`
-  margin-top: 40px;
   color: #0e0e0e;
-  font-size: 28px;
+  font-size: 1.75rem;
   font-style: normal;
-  font-weight: 700;
-  line-height: 150%; /* 42px */
+  font-weight: 600;
+  line-height: 150%;
   @media (max-width: 1024px) {
     font-size: 1.125rem;
     line-height: 120%;
