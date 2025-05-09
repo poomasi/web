60ee7e207a0d902c7928b8cebdae7551e44116e2
jeongman <gongmin779@gmail.com>
2025-05-09 16:14:29 +0900

    refactor: debounce 관련 작업 가이드 커밋

--- src/components/button/debounced-button.tsx
+++ src/components/button/debounced-button.tsx
@@ -1,36 +1,39 @@
-import { useEffect, useRef } from 'react'
-import { fromEvent } from 'rxjs'
-import { debounceTime } from 'rxjs/operators'
+import { useRef } from 'react'
 import Button from '@mui/material/Button'
 import type { SxProps, Theme } from '@mui/material'
 
 interface DebouncedButtonProps {
   text: string
-  onClick: () => void
+  onClick: () => Promise<void>;
   variant?: string
   sx?: SxProps<Theme>
   disabled?: boolean
 }
 
 export const DebouncedButton = ({ text, onClick, variant, sx, disabled }: DebouncedButtonProps) => {
-  const buttonRef = useRef<HTMLButtonElement>(null)
+  const clickEventRef = useRef<boolean>(false)
 
-  useEffect(() => {
-    if (!buttonRef.current) return
+  const handleButtonClick = async () => {
+    // 클릭 이벤트 실행 중인 경우, return
+    if (clickEventRef.current) return
+    // 클릭 이벤트 pending 임의 구현
+    clickEventRef.current = true
 
-    const clickObservable = fromEvent(buttonRef.current, 'click').pipe(debounceTime(500))
-
-    const subscription = clickObservable.subscribe(() => {
-      onClick()
-    })
-
-    return () => {
-      subscription.unsubscribe()
+    try {
+      await onClick()
+    } catch {
+      // 에러 처리가 필요하진 않을 것 같으나 참고만 부탁드립니다.
+    } finally {
+      // 클릭 이벤트 완료 후 0.5 초 뒤에 실행할 수 있도록 함
+      setTimeout(() => {
+        clickEventRef.current = false
+      }, 1000 * 0.5)
     }
-  }, [onClick])
+  }
 
   return (
-    <Button ref={buttonRef} variant={variant as 'text' | 'outlined' | 'contained'} sx={sx} disabled={disabled}>
+    <Button variant={variant as 'text' | 'outlined' | 'contained'} sx={sx} disabled={disabled}
+            onClick={handleButtonClick}>
       {text}
     </Button>
   )
