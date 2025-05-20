e2e579196293b0e5ea7359956b377bfa2083f35d
taetae070 <taetaeky07@gmail.com>
2025-05-20 20:43:29 +0900

    refactor: 전공 여부 및 개발 경력 enum 옵션 상수화

--- src/utils/api/enums.ts
+++ src/utils/api/enums.ts
@@ -12,16 +12,28 @@ export enum AccountType {
 }
 
 export enum CareerYearType {
-  ACADEMIC = 'U',
-  JOB_SEEKER = 'R',
-  JUNIOR = 'N',
-  MIDDLE = 'S',
+  ACADEMIC = 'ACADEMIC',
+  JOB_SEEKER = 'JOB_SEEKER',
+  JUNIOR = 'JUNIOR',
+  MIDDLE = 'MIDDLE',
 }
+
+export const CAREER_YEAR_OPTIONS = [
+  { value: CareerYearType.ACADEMIC, label: '대학생' },
+  { value: CareerYearType.JOB_SEEKER, label: '취준생' },
+  { value: CareerYearType.JUNIOR, label: '신입~3년차' },
+  { value: CareerYearType.MIDDLE, label: '3년차 이상' },
+]
 
 export enum AskerSpecificType {
   SPECIALTY = 'SPECIALTY',
   NONE_SPECIALTY = 'NONE_SPECIALTY',
 }
+
+export const SPECIFIC_TYPE_OPTIONS = [
+  { value: AskerSpecificType.SPECIALTY, label: '전공자' },
+  { value: AskerSpecificType.NONE_SPECIALTY, label: '비전공자' },
+]
 
 export enum QnaAskerType {
   ALL = 'ALL',
