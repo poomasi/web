2a8652987c5d54638f2685f26261b204d156445a
jeongman <gongmin779@gmail.com>
2025-06-13 17:15:15 +0900

    refactor: Header 이미지 가져오는 경로 수정

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -4,106 +4,105 @@ import Image from "next/image";
 import styled from "@emotion/styled";
 import { KakaoLogin } from "@utils/kakao-login";
 import { useAccountStore } from "@store/account";
-import publicLogo from "@images/public-logo.png";
 
 export function Header() {
-	const { accessToken, resetaccessToken } = useAccountStore();
-	const toHome = () => {
-		window.location.href = "/";
-	};
+  const { accessToken, resetaccessToken } = useAccountStore();
+  const toHome = () => {
+    window.location.href = "/";
+  };
 
-	const handleLogout = () => {
-		resetaccessToken();
-		window.location.reload();
-	};
+  const handleLogout = () => {
+    resetaccessToken();
+    window.location.reload();
+  };
 
-	return (
-		<>
-			<HeaderContainer>
-				<HeaderContent>
-					<LogoButtonWrapper onClick={toHome}>
-						<LogoImage
-							src={publicLogo}
-							alt="logo"
-							width={150}
-							height={30}
-						/>
-					</LogoButtonWrapper>
+  return (
+    <>
+      <HeaderContainer>
+        <HeaderContent>
+          <LogoButtonWrapper onClick={toHome}>
+            <LogoImage
+              src={"/images/public-logo.png"}
+              alt="logo"
+              width={150}
+              height={30}
+            />
+          </LogoButtonWrapper>
 
-					{accessToken ? (
-						<KakaoLoginBtn onClick={() => handleLogout()}>
-							로그아웃
-						</KakaoLoginBtn>
-					) : (
-						<KakaoLogin />
-					)}
-				</HeaderContent>
-			</HeaderContainer>
-		</>
-	);
+          {accessToken ? (
+            <KakaoLoginBtn onClick={() => handleLogout()}>
+              로그아웃
+            </KakaoLoginBtn>
+          ) : (
+            <KakaoLogin />
+          )}
+        </HeaderContent>
+      </HeaderContainer>
+    </>
+  );
 }
 
 const LogoButtonWrapper = styled.button`
-	cursor: pointer;
-	font-size: 40px;
-	background: none;
-	border: none;
-	padding: 0;
+  cursor: pointer;
+  font-size: 40px;
+  background: none;
+  border: none;
+  padding: 0;
 
-	@media (max-width: 1024px) {
-		font-size: 0;
-	}
+  @media (max-width: 1024px) {
+    font-size: 0;
+  }
 `;
 
 const LogoImage = styled(Image)`
-	@media (max-width: 1024px) {
-		width: 6.125rem;
-		height: 1.25rem;
-	}
+  @media (max-width: 1024px) {
+    width: 6.125rem;
+    height: 1.25rem;
+  }
 `;
 
 const KakaoLoginBtn = styled.button`
-	font-size: 1.1875rem;
-	padding: 0.6rem 1.25rem;
-	color: #08ae98;
-	background-color: #fff;
-	border-radius: 12px;
-	border: 1.5px solid #3ecdba;
+  font-size: 1.1875rem;
+  padding: 0.6rem 1.25rem;
+  color: #08ae98;
+  background-color: #fff;
+  border-radius: 12px;
+  border: 1.5px solid #3ecdba;
 
-	@media (max-width: 1024px) {
-		width: 5rem;
-		padding: 7px 12px;
-		height: 30px;
-		font-size: 12px;
-		border-radius: 6px;
-	}
+  @media (max-width: 1024px) {
+    width: 5rem;
+    padding: 7px 12px;
+    height: 30px;
+    font-size: 12px;
+    border-radius: 6px;
+  }
 `;
 
 const HeaderContainer = styled.div`
-	position: sticky;
-	width: 100%;
-	padding: 1rem 0;
-	top: 0;
-	display: flex;
-	justify-content: center;
-	color: #333;
-	background-color: #fff;
-	z-index: 999;
+  position: sticky;
+  width: 100%;
+  padding: 1rem 0;
+  top: 0;
+  display: flex;
+  justify-content: center;
+  color: #333;
+  background-color: #fff;
+  z-index: 999;
 
-	@media (max-width: 1024px) {
-		width: 100%;
-		padding: 12px 0;
-	}
+  @media (max-width: 1024px) {
+    width: 100%;
+    padding: 12px 0;
+  }
 `;
 const HeaderContent = styled.div`
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	width: 100%;
-	max-width: 1320px;
-	margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 1320px;
+  margin: 0 auto;
 
-	@media (max-width: 1320px) {
-		padding: 0 5%;
-	}
+  @media (max-width: 1320px) {
+    padding: 0 5%;
+  }
 `;
