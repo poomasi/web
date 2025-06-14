892523233297672166a97ae8a2f3094480ff6571
jeongman <gongmin779@gmail.com>
2025-06-14 21:13:07 +0900

    refactor: 로그인 안 한 상태에서 품앗이꾼 카드 클릭 시 라우터 이동하는 문제 수정

--- src/hooks/landingPage/useProfileCard.ts
+++ src/hooks/landingPage/useProfileCard.ts
@@ -7,35 +7,36 @@ import { useAccountStore } from "@store/account";
 import { useMobileStore } from "@store/useMobileStore";
 
 export function useProfileCard() {
-	const router = useRouter();
-	const { accessToken } = useAccountStore((state) => state);
-	const isMobile = useMobileStore((state) => state.isMobile);
+  const router = useRouter();
+  const { accessToken } = useAccountStore((state) => state);
+  const isMobile = useMobileStore((state) => state.isMobile);
 
-	// const [useGuideModal, setUseGuideModal] = useState(false); //모달띄울지말지 결정
-	const [selectedCardKey, setSelectedCardKey] = useState<
-		"WebInstructions" | "MobileInstructions" | null
-	>(null);
+  // const [useGuideModal, setUseGuideModal] = useState(false); //모달띄울지말지 결정
+  const [selectedCardKey, setSelectedCardKey] = useState<
+    "WebInstructions" | "MobileInstructions" | null
+  >(null);
 
-	const handleProfileClick = (profile: ProfileData) => {
-		if (accessToken === null && isMobile) {
-			// setUseGuideModal(true);
-			setSelectedCardKey("MobileInstructions");
-			return;
-		}
-		if (accessToken === null && !isMobile) {
-			setSelectedCardKey("WebInstructions");
-		}
+  const handleProfileClick = (profile: ProfileData) => {
+    if (accessToken === null && isMobile) {
+      // setUseGuideModal(true);
+      setSelectedCardKey("MobileInstructions");
+      return;
+    }
+    if (accessToken === null && !isMobile) {
+      setSelectedCardKey("WebInstructions");
+      return;
+    }
 
-		if (!profile.is_vacation) {
-			router.push(`/${profile.nickname}`);
-		}
-	};
+    if (!profile.is_vacation) {
+      router.push(`/${profile.nickname}`);
+    }
+  };
 
-	return {
-		handleProfileClick,
-		// useGuideModal,
-		// setUseGuideModal,
-		selectedCardKey,
-		setSelectedCardKey,
-	};
+  return {
+    handleProfileClick,
+    // useGuideModal,
+    // setUseGuideModal,
+    selectedCardKey,
+    setSelectedCardKey,
+  };
 }
