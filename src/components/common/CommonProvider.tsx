ea3bc4b5b62026d8b9f3193cb61b5316e2f55f59
taetae070 <taetaeky07@gmail.com>
2025-06-18 22:10:41 +0900

    fix: hydration mismatch 에러 고침

--- src/components/common/CommonProvider.tsx
+++ src/components/common/CommonProvider.tsx
@@ -1,59 +1,68 @@
 "use client";
 
 import {
-  QueryCache,
-  QueryClient,
-  QueryClientProvider,
+	QueryCache,
+	QueryClient,
+	QueryClientProvider,
 } from "@tanstack/react-query";
 import { globalTheme } from "@styles/global-theme.ts";
 import { ThemeProvider } from "@mui/material";
 import { GlobalStyle } from "@styles/GlobalStyle.tsx";
 import { Toast } from "@components/toast/Toast.tsx";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useMobileStore } from "@store/useMobileStore.ts";
 
 interface CommonLayoutProps {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 export function CommonProvider({ children }: CommonLayoutProps) {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {},
-    },
-    queryCache: new QueryCache({
-      onError: (error, query) => {
-        console.log("Query Error Details:", {
-          error,
-          queryKey: query.queryKey,
-          queryHash: query.queryHash,
-          state: query.state,
-        });
-      },
-    }),
-  });
+	//QueryClient: React Query에서 서버 상태를 관리할 중심 객체
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {},
+		},
+		//queryCache: 모든 쿼리의 캐시 상태를 관리하는 객체
+		queryCache: new QueryCache({
+			onError: (error, query) => {
+				console.log("Query Error Details:", {
+					error,
+					queryKey: query.queryKey,
+					queryHash: query.queryHash,
+					state: query.state,
+				});
+			},
+		}),
+	});
 
-  const { setIsMobile } = useMobileStore();
-  const sizeCheckEvent = () => {
-    setIsMobile(window.innerWidth <= 1024);
-  };
+	const { setIsMobile } = useMobileStore();
+	const [hasMounted, setHasMounted] = useState(false);
 
-  useEffect(() => {
-    sizeCheckEvent();
-    window.addEventListener("resize", sizeCheckEvent);
+	useEffect(() => {
+		const sizeCheckEvent = () => {
+			setIsMobile(window.innerWidth <= 1024);
+		};
 
-    return () => {
-      window.removeEventListener("resize", sizeCheckEvent);
-    };
-  }, []);
+		sizeCheckEvent();
+		window.addEventListener("resize", sizeCheckEvent);
 
-  return (
-    <ThemeProvider theme={globalTheme}>
-      <QueryClientProvider client={queryClient}>
-        <GlobalStyle />
-        {children}
-        <Toast />
-      </QueryClientProvider>
-    </ThemeProvider>
-  );
+		setHasMounted(true);
+
+		return () => {
+			window.removeEventListener("resize", sizeCheckEvent);
+		};
+	}, []);
+
+	if (!hasMounted) return null; // hydration mismatch 방지
+
+	return (
+		<ThemeProvider theme={globalTheme}>
+			{/* QueryClientProvider에 넘겨줘야 React Query 기능들이 작동 */}
+			<QueryClientProvider client={queryClient}>
+				<GlobalStyle />
+				{children}
+				<Toast />
+			</QueryClientProvider>
+		</ThemeProvider>
+	);
 }
