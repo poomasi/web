0c6149dd5e891755db985000d773d246352eed9e
jeongman <gongmin779@gmail.com>
2025-06-21 20:59:08 +0900

    refactor: Next js 로컬 폰트 적용되도록 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Footer } from "@components/common/Footer/Footer";
 import { Header } from "@components/common/Header/Header";
 import { CommonProvider } from "@components/common/CommonProvider.tsx";
+import { suitFont } from "./font.ts";
 import "./globals.css";
 
 export default function RootLayout({
@@ -9,7 +10,7 @@ export default function RootLayout({
   children: React.ReactNode; //타입명시
 }) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={`${suitFont.variable} ${suitFont.className}`}>
       <body>
         <CommonProvider>
           <Header />
