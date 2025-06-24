f4cbda61f7bbd054aae26b542d6af93f6ebdfb38
taetae070 <taetaeky07@gmail.com>
2025-06-24 23:56:06 +0900

    setting...

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@
 
 #!/bin/sh
 
-mkdir -p output
-cp -R ./src ./public ./package.json ./next.config.mjs ./tsconfig.json ./yarn.lock ./output
+# mkdir -p output
+# cp -R ./src ./public ./package.json ./next.config.mjs ./tsconfig.json ./yarn.lock ./output
 
 
-# cd ../
-# mkdir output
-# cp -R ./web/* ./output
-# cp -R ./output ./web/
+cd ../
+mkdir output
+cp -R ./web/* ./output
+cp -R ./output ./web/
 
 # mkdir -p output
 # cp -R ./* ./output
