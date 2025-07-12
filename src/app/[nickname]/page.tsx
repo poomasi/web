2ccfa5704458c5455d82b27fe386ad20becc5d19
taetae070 <taetaeky07@gmail.com>
2025-07-12 17:28:55 +0900

    refactor: 질문페이지 초기 로딩 성능 개선을 위한 코드 스플리팅

--- src/app/[nickname]/page.tsx
+++ src/app/[nickname]/page.tsx
@@ -1,15 +1,9 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styled from "@emotion/styled";
-import { useParams, useRouter } from "next/navigation";
-import { useAccountStore } from "@store/index.ts";
-import { useToastMessageStore } from "@toast";
-import { useDetailPageContext } from "@hooks/qnaPage/provider/DetailPageProvider.tsx";
-import { useMobileStore } from "@store/useMobileStore.ts";
-import { isAxiosError } from "axios";
-import { AccountType, RequestApi } from "@api/index.ts";
 import dynamic from "next/dynamic";
+import { useQnaPage } from "@hooks/qnaPage/useQnaPage";
 
 const TeacherIntroduce = dynamic(
 	() =>
@@ -52,75 +46,7 @@ const MobileQnaPage = dynamic(
 
 // 실제 QnaPage 컴포넌트 (Context 소비자)
 export default function QnaPageContent() {
-	useEffect(() => {
-		const switchStart = Number(localStorage.getItem("QnaPage_start_time"));
-
-		if (switchStart) {
-			const switchEnd = Date.now();
-			/*console.log(
-        "프로필 카드 클릭 → 페이지 도착까지 걸린 시간(ms):",
-        switchEnd - switchStart,
-      );*/
-			localStorage.removeItem("QnaPage_start_time");
-		}
-	}, []);
-
-	const router = useRouter();
-	const params = useParams();
-	const id = params?.nickname as string;
-
-	const { publicId, accountType } = useAccountStore();
-	const { setErrorToastMessage } = useToastMessageStore();
-	const { isMobile } = useMobileStore();
-	const { pageLoading, setTeacherAccount, setPageLoading, teacherAccount } =
-		useDetailPageContext();
-
-	const [isAnswerAuthority, setIsAnswerAuthority] = useState<boolean>(false);
-
-	const handleError = (message: string) => {
-		setErrorToastMessage(message);
-		router.push("/");
-	};
-
-	const getTeacherData = async () => {
-		if (!id) {
-			handleError("잘못된 접근입니다.");
-			return;
-		}
-
-		try {
-			const res = await RequestApi.accounts.getAccount(id);
-			setTeacherAccount(res.data);
-			setPageLoading(true);
-		} catch (error) {
-			if (isAxiosError(error) && error.response?.status === 401) {
-				handleError("로그인 토큰이 만료되었습니다. 다시 로그인 해주세요.");
-			} else {
-				handleError("품앗이꾼 정보를 가져오는 데 실패했습니다.");
-			}
-		}
-	};
-
-	useEffect(() => {
-		if (!publicId) {
-			handleError("로그인을 먼저 진행해주세요.");
-			return;
-		}
-
-		scroll(0, 0);
-		getTeacherData();
-	}, [publicId]);
-
-	useEffect(() => {
-		if (teacherAccount) {
-			setIsAnswerAuthority(
-				teacherAccount.public_id === publicId &&
-					accountType === AccountType.MENTOR
-			);
-		}
-	}, [teacherAccount]);
-
-	// console.log("isMobile", isMobile);
+	const { isMobile, pageLoading, isAnswerAuthority } = useQnaPage();
 
 	return isMobile ? (
 		<MobileQnaPage />
