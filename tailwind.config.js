478dfe6c9ee6c5f39cb9904bab7dc69175ade8ee
jeongman <gongmin779@gmail.com>
2025-08-04 22:16:09 +0900

    feat: tailwind 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,31 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
-  content: [],
+  mode: "jit",
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      xl: "1440px",
+    },
+    extend: {
+      animation: {
+        rolling_1: "10s linear infinite normal none running rolling_1",
+      },
+      keyframes: {
+        rolling_1: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
