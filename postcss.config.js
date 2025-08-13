183503027deefc4440dc2d49b7cde86f60d61412
taetae070 <taetaeky07@gmail.com>
2025-08-13 22:53:37 +0900

    fix: tailwind css 적용안되는 문제 해결중01

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,3 @@
-// module.exports = {
-// 	plugins: {
-// 		"@tailwindcss/postcss": {},
-// 		autoprefixer: {},
-// 	},
-// };
 module.exports = {
 	plugins: {
 		"@tailwindcss/postcss": {},
---
+++ tailwind.config.ts
@@ -0,0 +1,32 @@
+/** @type {import("tailwindcss").Config} */
+const config = {
+	content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+	theme: {
+		screens: {
+			sm: "640px",
+			md: "768px",
+			xl: "1440px",
+		},
+		extend: {
+			animation: {
+				rolling_1: "30s linear infinite normal none running rolling_2",
+				rolling_2: "30s linear infinite rolling_2",
+			},
+			keyframes: {
+				rolling_1: {
+					"0%": { transform: "translateX(100%)" },
+					"50%": { transform: "translateX(-100%)" },
+					"50.1%": { transform: "translateX(100%)" },
+					"100%": { transform: "translateX(0%)" },
+				},
+				rolling_2: {
+					"0%": { transform: "translateX(0%)" },
+					"100%": { transform: "translateX(-200%)" },
+				},
+			},
+		},
+	},
+	plugins: [],
+};
+
+export default config;
